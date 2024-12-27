5c114b49b2dd5d6225e2bd39da43f41bd2c02935
thatjuancodes <thejuan.codes@gmail.com>
2024-12-27 11:37:35 +0700

    add status data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,16 @@
 class HomeController < ApplicationController
   def index
     @users = [
-      { first_name: "Alice", last_name: "Schtark", email: "alice@example.com", phone: "9814729142" },
-      { first_name: "Bob", last_name: "Denboe", email: "bob@example.com", phone: "9814729242" },
-      { first_name: "Charlie", last_name: "Tarkis", email: "charlie@example.com", phone: "9814259142" },
-      { first_name: "David", last_name: "Smith", email: "david@example.com", phone: "9814729342" },
-      { first_name: "Eve", last_name: "Johnson", email: "eve@example.com", phone: "9814729442" },
-      { first_name: "Frank", last_name: "Brown", email: "frank@example.com", phone: "9814729542" },
-      { first_name: "Grace", last_name: "Davis", email: "grace@example.com", phone: "9814729642" },
-      { first_name: "Hank", last_name: "Miller", email: "hank@example.com", phone: "9814729742" },
-      { first_name: "Ivy", last_name: "Wilson", email: "ivy@example.com", phone: "9814729842" },
-      { first_name: "Jack", last_name: "Moore", email: "jack@example.com", phone: "9814729942" }
+      { first_name: "Alice", last_name: "Schtark", email: "alice@example.com", phone: "9814729142", status: true },
+      { first_name: "Bob", last_name: "Denboe", email: "bob@example.com", phone: "9814729242", status: false },
+      { first_name: "Charlie", last_name: "Tarkis", email: "charlie@example.com", phone: "9814259142", status: true  },
+      { first_name: "David", last_name: "Smith", email: "david@example.com", phone: "9814729342", status: true  },
+      { first_name: "Eve", last_name: "Johnson", email: "eve@example.com", phone: "9814729442", status: false  },
+      { first_name: "Frank", last_name: "Brown", email: "frank@example.com", phone: "9814729542", status: false  },
+      { first_name: "Grace", last_name: "Davis", email: "grace@example.com", phone: "9814729642", status: false  },
+      { first_name: "Hank", last_name: "Miller", email: "hank@example.com", phone: "9814729742", status: true  },
+      { first_name: "Ivy", last_name: "Wilson", email: "ivy@example.com", phone: "9814729842", status: true  },
+      { first_name: "Jack", last_name: "Moore", email: "jack@example.com", phone: "9814729942", status: true  }
     ]
 
     if params[:filter].present?
