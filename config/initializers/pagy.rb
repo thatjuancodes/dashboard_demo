950b607231c8b2b5662a1f2083705761bba17fef
thatjuancodes <thejuan.codes@gmail.com>
2024-12-27 11:43:51 +0700

    fix lint issues

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,3 +1,3 @@
 Pagy::DEFAULT[:limit] = 3
-require 'pagy/extras/bootstrap' # items per page
-require 'pagy/extras/array'
+require "pagy/extras/bootstrap" # items per page
+require "pagy/extras/array"
